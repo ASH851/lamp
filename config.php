08a956358a519f52ad863e329f05afa113f76f06
Ashwini Kumar Tiwari <158164174+ASH851@users.noreply.github.com>
2025-01-28 21:00:10 +0530

    config.php

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
-$servername = getenv('DB_HOST');  // DB_HOST should already include /cloudsql/[INSTANCE_CONNECTION_NAME]
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$dbname = getenv('DB_NAME');
+$servername = getenv('DB_HOST'); // MySQL host
+$username = getenv('DB_USER'); // MySQL username
+$password = getenv('DB_PASS'); // MySQL password
+$dbname = getenv('DB_NAME'); // MySQL database
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+echo "Connected successfully to MySQL database!";
 ?>
